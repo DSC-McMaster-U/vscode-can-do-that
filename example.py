b361a2c16dff3e75a674ea3666bb04a61a1883d0
jaymody <jaykmody@gmail.com>
2021-01-27 00:23:29 -0500

    simplified example.py

--- example.py
+++ example.py
@@ -1,18 +1,7 @@
-"""Computes if a given number can be expressed as the sum of two fibonnaci numbers."""
+"""Checks if a given number can be expressed as the sum of two fibonnaci numbers."""
 
 def fib(n):
-    """Computes the first n numbers of the fibonnaci sequence.
-
-    Args
-    ----
-    n : int
-        How many fibonnaci numbers to compute (including n)
-
-    Returns
-    -------
-    list[int]
-        The first n numbers of the fibonnaci sequence
-    """
+    """Computes the fibonnaci sequence up to and including n."""
     if n == 0:
         return [0]
 
@@ -22,21 +11,7 @@ def fib(n):
     return fib_seq
 
 def two_sum(nums, target):
-    """Checks if there exists a pair of numbers in nums that sum to target.
-
-    Args
-    ----
-    nums : list[int]
-        List of numbers
-    target : int
-        Target sum
-
-    Returns
-    -------
-    bool
-        Returns True if two different elements in nums sums to target,
-        else returns False
-    """
+    """Checks if there exists a pair of numbers in nums that sums to target."""
     for i in range(len(nums)):
         for j in range(i, len(nums)):
             if (nums[i] + nums[j]) == target:
@@ -49,6 +24,7 @@ def test_two_sum():
     assert two_sum([4, 2, 3, 1], 100) == False
     assert two_sum([4, 2, 3, 1], 8) == False
 
+# main code
 test_two_sum()
 print("tests passed")
 
@@ -61,40 +37,53 @@ print("nums =", fibonacci_seq)
 print("result =", result)
 
 
+####################
+### Things To Do ###
+####################
+"install python extension for language features like intellisense,"
+"code completition, symbols, go to definition, debugging, etc ..."
 
-### todo ###
-# install python extension for language
-# features like intellisense, code completition, symbols, go to definition, debugging, run, etc ...
+"run using the run button"
+"run using integrated terminal"
 
-# run using the run button
-# run using integrated terminal
+"debug code"
 
-# debug code
+"stage and commit edit.py using git gui"
+"stage and commit navigate.js using integrated terminal"
+"wrap code in __main__ using code snippet"
+"demonstrate git gutter diff"
+"demonstrate a complete git diff of the file"
+"stage and commit fix and addition of __main__ in seperate commits"
 
-# commit one of the previous files using git gui
-# commit another of the previous files using terminal git
-# stage the above fix using the git gutter
-# git diff with previous version
+"add command line arguments for --test, --n, and --target"
+"demonstrate code snippet for parser and add_argument"
 
-# wrap code in __main__ using code snippet
-# add command line arguments for --test, --n, and --target
-# add custom code snippet for argparse
+"add linting (pylint)"
+"add formatting (black)"
 
-# add linting (pylint)
-# add formatting (black)
+"see settings.json under .vscode"
+"see launch.json"
+"see tasks.json"
 
-# see launch.json (more debugging configurations)
-# see tasks.json (build tasks, testing tasks, code quality, etc ...)
+"""
+useful settings
+- auto format on save
+- auto trim trailing whitespace on save
+- auto add new line to end of file
+- editor ruler at 80
+- for more editing space, toggle
+    - status bar
+    - minimap
+    - breadcrumbs
+    - side bar
+    - activity bar
+"""
 
-# useful settings
-# - auto format on save
-# - auto trim trailing whitespace on save
-# - auto add new line to end of file
-# - editor ruler at 80
-# - language specific settings
-# - for more editing space, toggle
-#     - status bar
-#     - minimap
-#     - breadcrumbs
-#     - side bar
-#     - activity bar
+"""
+    "files.trimTrailingWhitespace": true,
+    "editor.formatOnSave": true,
+    "files.insertFinalNewline": true,
+    "editor.rulers": [
+        80
+    ],
+"""
